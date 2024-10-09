d393a9368926495c9b00ce648bbd7c2b7d8bb3ff
shia25 <shiabhasan159@gmail.com>
2024-10-09 23:49:46 +0100

    bcrypt setup on user

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,41 @@
 const express = require("express")
 const router = express.Router()
 
+//bcrypt securty setting
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+//user password input
+const plainPassword = req.body.password;
+
+
+// GET route to show the registration form
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
 })    
 
+// POST route to handle the registration form submission
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
-})
+
+    // Extracting user details from register form submission
+    const plainPassword = req.body.password; 
+    const first = req.body.first; 
+    const last = req.body.last; 
+    const email = req.body.email; 
+
+    // Hash the password before storing it
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        if (err) {
+            return res.status(500).send("Error hashing the password");
+        }
+        // Send response (or store in the database)
+        // using template literals for readability and maintainability
+        res.send(`Hello ${first} ${last}, you are now registered! We will send an email to you at ${email}. Your hashed password is: ${hashedPassword}`);
+    });
+});
+
+
+
 
 // Export the router object so index.js can access it
 module.exports = router

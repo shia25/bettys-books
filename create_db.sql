bf6f7c1444b8b7ddbe4cc0c276b09027232c8b37
shia25 <shiabhasan159@gmail.com>
2024-10-10 00:18:25 +0100

    users table added

--- create_db.sql
+++ create_db.sql
@@ -7,6 +7,16 @@ USE bettys_books;
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
+# Create user table
+ CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        hashedPassword VARCHAR(255) NOT NULL,
+    );
+
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';

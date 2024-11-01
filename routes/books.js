6b253d7af5caf89aa0a58cc7a1e8c38b212d4482
shia25 <shiabhasan159@gmail.com>
2024-11-01 21:41:54 +0000

    sanitization added on books with some improvement on sql query format

--- routes/books.js
+++ routes/books.js
@@ -1,3 +1,5 @@
+// Import express-validator checks
+const {check, validationResult} = require('express-validator')
 const express = require("express")
 const router = express.Router()
 
@@ -16,16 +18,23 @@ router.get('/search',function(req, res, next){
 })
 
 
-router.get('/search_result', function (req, res, next) {
-    // Search the database
-    let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.search_text + "%'" // query database to get all the books
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.render("list.ejs", {availableBooks:result})
-     }) 
+router.get('/search_result',  [
+    //sanitization on search
+    check('search_text').trim().escape()],
+    function (req, res, next) {
+
+    // Validation error handling ?
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Search the database ?
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    db.query(sqlquery, [`%${req.query.search_text}%`], (err, result) => {
+        if (err) return next(err);
+        res.render("list.ejs", { availableBooks: result });
+    });
 })
 
 
@@ -44,18 +53,24 @@ router.get('/addbook', redirectLogin, function (req, res, next) {
     res.render('addbook.ejs')
 })
 
-router.post('/bookadded', function (req, res, next) {
+router.post('/bookadded', [
+    check('name').trim().escape().isLength({ min: 2 }).withMessage('Name must be at least 2 characters long'),
+    check('price').isNumeric().withMessage('Price must be a number')],
+    function (req, res, next) {
+
+    // Validation error handling
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     // saving data in database
-    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)";
     // execute sql query
-    let newrecord = [req.body.name, req.body.price]
-    db.query(sqlquery, newrecord, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        else
-            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
-    })
+    db.query(sqlquery, [req.body.name, req.body.price], (err, result) => {
+        if (err) return next(err);
+        // backtick string for reading string intrapolation
+        res.send(`This book is added to database, name: ${req.body.name}, price: £ ${req.body.price}`);
+    });
 }) 
 
 router.get('/bargainbooks', redirectLogin, function(req, res, next) {
